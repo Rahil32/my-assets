00c527300b2b10047be9ac0aba4ba17e102a2bca
Rahil32 <rahiltopno32@gmail.com>
2022-07-25 16:47:33 +0530

    component

---
+++ src/Components/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Login() {
+    return(
+    <div className="ui for login">
+        <h1 login = "Login">Login</h1>
+    </div>
+    )
+}
+
+export default Login
