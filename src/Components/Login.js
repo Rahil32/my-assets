4a2691621a17ad7740292ae44478a694911d0740
Rahil32 <rahiltopno32@gmail.com>
2022-07-25 16:52:53 +0530

    class component

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React,{Component} from "react";
 
-function Login() {
-    return(
-    <div className="ui for login">
-        <h1 login = "Login">Login</h1>
-    </div>
-    )
+class Login extends Component{
+    render(){
+        return(
+            <div className="ui for login">
+                <h1 login = "Login">Login</h1>
+            </div>
+            )
+    }
 }
 
 export default Login
