b64438b48f1ebe45c06e6b621987df30c865e136
Rahil32 <rahiltopno32@gmail.com>
2022-07-25 17:05:15 +0530

    with jsx

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,6 +1,9 @@
 import React from "react";
 
 function Login(){
+    /* with jsx render(
+        <div>hello world</div>
+        )*/
         return React.createElement("h1",null,"hello world") // w/o jsx
     }
 
